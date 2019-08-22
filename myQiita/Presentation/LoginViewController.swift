159f487e7462bbadfe68d3a726c3eff6e7588f7b
a.naga <shinamonphone@gmail.com>
2019-08-22 20:18:38 +0900

    [add] make my firstViw

---
+++ myQiita/Presentation/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  myQiita
+//
+//  Created by aimon on 2019/08/22.
+//  Copyright © 2019 a.naga. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myQiita/Util/ColorUtil.swift
@@ -0,0 +1,14 @@
+//
+//  ColorUtil.swift
+//  myQiita
+//
+//  Created by aimon on 2019/08/22.
+//  Copyright © 2019 a.naga. All rights reserved.
+//
+import UIKit
+
+struct ColorUtil {
+    
+    static let mainGreen  = UIColor.init(red: 114.0, green: 193.0, blue: 58.0, alpha: 1.0)
+    
+}
